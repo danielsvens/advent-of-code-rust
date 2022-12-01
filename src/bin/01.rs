c9333d1dc67c4d85ab73bc626f7f2f1e56f24282
Daniel Svens <danielsvens@hotmail.com>
2022-12-01 23:35:06 +0100

    Day one

---
+++ src/bin/01.rs
@@ -0,0 +1,68 @@
+use advent_of_code::helpers::vec_of_strings;
+use itertools::max;
+
+
+fn parse(num: &str) -> u32 {
+    match num.parse::<u32>() {
+        Ok(n) => n,
+        Err(_) => 0,
+      }
+}
+
+fn calc(input: &str) -> Vec<u32> {
+    let strings = vec_of_strings(&input);
+    let mut nums: Vec<u32> = Vec::new();
+    let mut holder: u32 = 0;
+    let mut temp;
+
+    for n in strings {
+        temp = parse(&n);
+
+        if temp == 0 {
+            nums.push(holder);
+            holder = 0;
+        }
+
+        holder += temp
+    }
+
+    nums.push(holder);
+
+    nums
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    max(calc(input))
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut nums = calc(input);
+    nums.sort_by(|a, b| b.cmp(a));
+    let result = &nums[0..3];
+    let sliced = result.to_vec();
+    let summa = sliced.iter().sum();
+    Some(summa)
+}
+
+ fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(24000));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), Some(45000));
+    }
+}
--- src/helpers.rs
+++ src/helpers.rs
@@ -2,3 +2,12 @@
  * Use this file if you want to extract helpers from your solutions.
  * Example import from this file: `use advent_of_code::helpers::example_fn;`.
  */
+
+pub fn vec_of_strings(input: &str) -> Vec<&str> {
+    input.split('\n').map(|str| str.trim()).collect()
+}
+
+pub fn vec_of_numbers(input: &str) -> Vec<u32> {
+    let strings = vec_of_strings(input);
+    strings.iter().map(|str| str.parse().unwrap()).collect()
+}
