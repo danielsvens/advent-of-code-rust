478daebe063d2e2057f3cfb6043b55326d369133
Daniel Svens <danielsvens@hotmail.com>
2022-12-05 23:01:07 +0100

    Day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -17,9 +17,26 @@ fn take(mut containers: Vec<String>, times: usize, from: usize, to: usize) -> Ve
     containers
 }
 
-pub fn part_one(input: &str) -> Option<String> {
-    let strings = vec_of_strings(input);
-    let mut stacks: Vec<String> = vec![
+fn take_multiple(mut containers: Vec<String>, times: usize, from: usize, to: usize) -> Vec<String> {
+    let mut cache = String::from("");
+
+    for _ in 0..times {
+        let _item = containers[from - 1].chars().last().unwrap();
+        let _last = containers[from - 1].len();
+        
+        containers[from - 1].remove(_last - 1);
+        cache.push(_item);
+    };
+
+    for c in cache.chars().rev() {
+        containers[to - 1].push(c);
+    }
+
+    containers
+}
+
+fn setup_input() -> Vec<String> {
+    vec![
         String::from("HCR"), 
         String::from("BJHLSF"), 
         String::from("RMDHJTQ"), 
@@ -29,30 +46,55 @@ pub fn part_one(input: &str) -> Option<String> {
         String::from("SNVZBPWL"),
         String::from("RJQGC"),
         String::from("LDTRHPFS")
-    ];
+    ]
+}
+
+fn get_args(separator: &Regex, instruction: &String) -> Vec<usize> {
+    separator.find_iter(instruction)
+            .map(|m| m.as_str().to_string())
+            .map(|e| parse_str_to_usize(&e))
+            .collect()
+}
+
+fn get_result(stacks: &Vec<String>) -> Vec<String> {
+    stacks.iter()
+        .map(|e| e.chars().last().unwrap())
+        .map(|e| e.to_string())
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<String> {
+    let strings = vec_of_strings(input);
+    let mut stacks: Vec<String> = setup_input();
 
     let separator = Regex::new(r"\d+").unwrap();
     let instructions = get_instuctions(&strings);
 
     for instruction in instructions {
-        let parsed: Vec<usize> = separator.find_iter(&instruction)
-            .map(|m| m.as_str().to_string())
-            .map(|e| parse_str_to_usize(&e))
-            .collect();
-
-        stacks = take(stacks, parsed[0], parsed[1], parsed[2]);
+        let args = get_args(&separator, &instruction);
+        stacks = take(stacks, args[0], args[1], args[2]);
     };
 
-    let result: Vec<String> = stacks.iter()
-        .map(|e| e.chars().last().unwrap())
-        .map(|e| e.to_string())
-        .collect();
+    let result: Vec<String> = get_result(&stacks);
 
     Some(result.join(""))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let strings = vec_of_strings(input);
+    let mut stacks: Vec<String> = setup_input();
+
+    let separator = Regex::new(r"\d+").unwrap();
+    let instructions = get_instuctions(&strings);
+
+    for instruction in instructions {
+        let args = get_args(&separator, &instruction);
+        stacks = take_multiple(stacks, args[0], args[1], args[2]);
+    };
+
+    let result: Vec<String> = get_result(&stacks);
+
+    Some(result.join(""))
 }
 
 fn main() {
@@ -74,6 +116,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(String::from("LSFJBGLCS")));
     }
 }
